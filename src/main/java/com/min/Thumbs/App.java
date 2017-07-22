3f215bdd7b540903820e454def606ba623ef1ff6
PC-20170304HXPM\Administrator <654279133@qq.com>
2017-07-22 16:32:19 +0800

    创建

---
+++ src/main/java/com/min/Thumbs/App.java
@@ -0,0 +1,13 @@
+package com.min.Thumbs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
